c2d3c6b08784e2c14d23272614a4e1a2c8724bd2
DEV <47359837+DEVXIX@users.noreply.github.com>
2023-06-22 23:57:32 +0400

    go

--- script.js
+++ script.js
@@ -1,198 +1,87 @@
-function powerOff() {
-  window.close();
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+const particles = [];
+const particleCount = 250;
+const particlesPerSecond = 100;
+
+// Set canvas size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Generate random character
+function getRandomCharacter() {
+    const characters = '!@#$%^&*';
+    return characters.charAt(Math.floor(Math.random() * characters.length));
 }
-document.addEventListener('DOMContentLoaded', function() {
-    function updateTime() {
-      const now = new Date();
-      const timeElement = document.getElementById('time');
-      const options = {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-      };
-      
-      let dateTimeString = now.toLocaleString(undefined, options);
-      dateTimeString = dateTimeString.replace('at', '').trim();
-      const [timeString, dateString] = dateTimeString.split(', ');
-      const dateOptions = {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric'
-      };
-      const dateStringFormatted = now.toLocaleDateString(undefined, dateOptions);
-      timeElement.innerHTML = `<div class="time">${timeString}</div><div class="date">${dateStringFormatted}</div>`;
-    }
-  
-    updateTime();
-    setInterval(updateTime, 1000);
 
-    const consoleText = document.querySelector('.console-text');
-    const consoleInput = document.querySelector('.console-input');
-    const consoleForm = document.querySelector('.console-prompt');
-    const consolePath = document.querySelector('.console-path');
-    const consoleUsername = document.querySelector('.console-username');
-    
-    let currentDirectory = '~';
-    
-    consoleInput.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        let command = consoleInput.value.trim();
-        consoleInput.value = '';
-        consoleLog(`${consoleUsername.textContent}${consolePath.textContent} ${command}`);
-        parseCommand(command);
-      }
-    });
-    
-    function parseCommand(command) {
-      let args = command.split(' ');
-      switch (args[0]) {
-        case 'ls':
-          if (currentDirectory === '~/Answers') {
-            consoleLog('1.txt, 2.txt, 3.txt, 4.txt, 5.txt, 6.txt');
-          } else {
-            consoleLog('Answers     index.html    style.css    script.js');
-          }
-          break;
-        case 'cd':
-          if (args[1] === '..') {
-            currentDirectory = '~';
-          } else if (args[1] === 'Answers') {
-            currentDirectory = '~/Answers';
-          } else {
-            consoleLog(`bash: cd: ${args[1]}: No such file or directory`);
-          }
-          consolePath.textContent = currentDirectory;
-          break;
-        case 'cat':
-          if (args[1]) {
-            fetch(`Answers/${args[1]}.txt`)
-              .then((response) => response.text())
-              .then((data) => {
-                consoleLog(`<pre class="language-python">${data}</pre>`, true);
-              })
-              .catch((error) => {
-                consoleLog(`Error: ${error}`);
-              });
-          }
-          break;
-        case 'about':
-          consoleLog(`Terminal Console v1.0
-          Built with HTML, CSS, and JavaScript.
-          Created by D3VXIX.`);
-          break;
-        case 'answers':
-          consoleLog(`Answers commands:
-              1- Arithmetic
-              2- Numbers
-              3- Wrong Check
-              4- Public Keys
-              5- Update Feed
-              6- Memo`);
-            break;
-        case 'help':
-          consoleLog(`
-          Type 'ls' to list the files in the current directory.
-          Type 'cd ..' to navigate to the parent directory.
-          Type 'answers' to see available commands.
-          Type 'cat [command number]' to view the answer Note: do not add the .txt .
-          Type 'about' to view information about the console`
-          );
-        case 'clear':
-          consoleText.innerHTML = '';
-          consoleLog(`Welcome to the console!
-    Type 'ls' to list the files in the current directory.
-    Type 'cd ..' to navigate to the parent directory.
-    Type 'answers' to see available commands.
-    Type 'cat [command number]' to view the answer of the Flag.
-    Type 'about' to view information about the console`
-    );
-          break;
-        default:
-          consoleLog(`bash: ${args[0]}: command not found`);
-      }
-    }
-    
-    function consoleLog(message) {
-      let consoleLine = document.createElement('div');
-      let formattedMessage = message
-        .replace(/(#[^\n]+)/g, '<span class="console-green">$1</span>') // modified regex to match whole line after '#'
-        .replace(/\b(from)\b/g, '<span class="console-purple">$1</span>')
-        .replace(/\b(import)\b/g, '<span class="console-purple">$1</span>')
-        .replace(/\b(print)\b/g, '<span class="console-orange">$1</span>')
-        .replace(/\b(def)\b/g, '<span class="console-blue">$1</span>')
-        .replace(/\(/g, '<span class="console-paren">(</span>')
-        .replace(/\)/g, '<span class="console-paren">)</span>')
-        .replace(/\]/g, '<span class="console-bracket">]</span>')
-        .replace(/\[/g, '<span class="console-bracket">[</span>')
-        .replace(/\b(if)\b/g, '<span class="console-purple">$1</span>')
-        .replace(/\b(return)\b/g, '<span class="console-green">$1</span>')
-        .replace(/\b(True)\b/g, '<span class="console-blue">$1</span>')
-        .replace(/\b(exit)\b/g, '<span class="console-bracket">$1</span>')
-    
-      consoleLine.innerHTML = formattedMessage;
-      consoleText.appendChild(consoleLine);
-      consoleText.scrollTop = consoleText.scrollHeight;
-    }
-    
-    consoleLog(`Welcome to the console!
-    Use the command [help] to get additional information about using this console.`);
-    
-    consolePath.textContent = currentDirectory;
-
-    const selectWindow = (window) => {
-      $('.window').removeClass('active');
-      $(`.window[data-window=${window}]`).addClass('active');
+// Particle class
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.character = getRandomCharacter();
     }
 
-    $('.window').draggable({
-      handle: 'header',
-      containment: ".windows",
-      start: function() {
-        selectWindow($(this).data('window'));
-      }
-    });
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
 
-    $('.window').click(function() {
-      selectWindow($(this).data('window'));
-    });
+        if (this.size > 0.2) this.size -= 0.1;
+    }
 
-    $('.start-icon').click(function() {
-      $('.menu-items').fadeToggle();
-    })
+    draw() {
+        ctx.font = this.size + '0.5em Arial';
+        ctx.fillStyle = 'white';
+        ctx.fillText(this.character, this.x, this.y);
+    }
+}
 
-    $('.console-close').click(function() {
-      $(this).parent().parent().parent().fadeOut();
-    })
+// Create particles
+function createParticles() {
+    for (let i = 0; i < particleCount; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particles.push(new Particle(x, y));
+    }
+}
 
-    $('#console-button').click(function() {
-      $('.console').fadeToggle();
-      selectWindow('console');
-    });
+// Add new particles
+function addParticles() {
+    for (let i = 0; i < particlesPerSecond; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particles.push(new Particle(x, y));
+    }
+}
 
-    $('#fm-button').click(function() {
-      $('.filemanager').fadeToggle();
-      selectWindow('filemanager');
-    });
+// Render particles
+function renderParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
 
-    $('#browser-button').click(function() {
-      $('.browser').fadeToggle();
-      selectWindow('browser');
-    });
+        if (particles[i].size <= 0.2) {
+            particles.splice(i, 1);
+            i--;
+        }
+    }
 
-    $('.icon-grid button').click(function() {
-      $('.icon-grid button').removeClass('selected');
-      $(this).addClass('selected');
-    });
+    requestAnimationFrame(renderParticles);
+}
 
-    $('.icon-grid button').blur(function() {
-      $(this).removeClass('selected');
-    });
+// Resize canvas on window resize
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
-    $('.icon-grid button').dblclick(function() {
-      const window = $(this).data('window');
-      $(`.${window}`).fadeIn();
-      selectWindow(window);
-    });
-    
-  });
+// Initialize and start animation
+createParticles();
+renderParticles();
+
+// Add new particles every 1000 / particlesPerSecond milliseconds
+setInterval(addParticles, 5000 / particlesPerSecond);
